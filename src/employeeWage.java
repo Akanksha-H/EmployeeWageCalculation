41220c1fc47977e25f1bd6a1a3cdadb72a36aae8
Aakanksha <aakanksha0499@gmail.com>
2022-05-17 00:44:32 +0530

    Added_Employee_Attendance

--- src/employeeWage.java
+++ src/employeeWage.java
@@ -1,6 +1,13 @@
 public class employeeWage {
+    // constant variables
+    static int DAY_CHECK = 1;
     public static void main(String[] args) {
         System.out.println("WELCOME_EMPLOYEE_TO_DAILY_WAGE_COMPUTATION");
+        // Computation
+        double empDailyCheck = Math.floor(Math.random() * 10) % 2;
+        if (empDailyCheck == DAY_CHECK)
+            System.out.println("Employee is Present Today");
+        else
+            System.out.println("Employee is Absent Today");
     }
-
 }
