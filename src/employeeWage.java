9adeea65ab5348745449f40a76762fc7fdb95f29
Aakanksha <aakanksha0499@gmail.com>
2022-05-17 01:09:32 +0530

    Added_Employee_Daily_Wage

--- src/employeeWage.java
+++ src/employeeWage.java
@@ -1,13 +1,16 @@
 public class employeeWage {
     // constant variables
+    static int DAILY_WORKING_HOURS = 8;
+    static int DAILY_WAGE = 20;
     static int DAY_CHECK = 1;
     public static void main(String[] args) {
         System.out.println("WELCOME_EMPLOYEE_TO_DAILY_WAGE_COMPUTATION");
+        int empDailyWage = DAILY_WAGE * DAILY_WORKING_HOURS;
         // Computation
         double empDailyCheck = Math.floor(Math.random() * 10) % 2;
         if (empDailyCheck == DAY_CHECK)
-            System.out.println("Employee is Present Today");
+            System.out.println("Employee is Present Today"+ "\r\n" +"Total Wage for Today = " + empDailyWage);
         else
-            System.out.println("Employee is Absent Today");
+            System.out.println("Employee is absent today");
     }
 }
