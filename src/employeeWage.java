b1f6f7357dfd7616ff55f84b3b228ef3f9d2b77e
Aakanksha <aakanksha0499@gmail.com>
2022-05-17 01:18:22 +0530

    Added_Employee_Part-Time_Wage

--- src/employeeWage.java
+++ src/employeeWage.java
@@ -1,16 +1,19 @@
 public class employeeWage {
     // constant variables
     static int DAILY_WORKING_HOURS = 8;
+    static int PART_TIME_HOURS = 4;
     static int DAILY_WAGE = 20;
-    static int DAY_CHECK = 1;
     public static void main(String[] args) {
         System.out.println("WELCOME_EMPLOYEE_TO_DAILY_WAGE_COMPUTATION");
         int empDailyWage = DAILY_WAGE * DAILY_WORKING_HOURS;
+        int empPartTimeWage = DAILY_WAGE * PART_TIME_HOURS;
         // Computation
-        double empDailyCheck = Math.floor(Math.random() * 10) % 2;
-        if (empDailyCheck == DAY_CHECK)
+        double empWage = Math.floor(Math.random() * 10) % 3;
+        if (empWage == 1)
             System.out.println("Employee is Present Today"+ "\r\n" +"Total Wage for Today = " + empDailyWage);
+        else if (empWage == 2)
+            System.out.println("Employee is Working Part Time today = " + empPartTimeWage);
         else
-            System.out.println("Employee is absent today");
+            System.out.println("Employee is Absent Today");
     }
 }
