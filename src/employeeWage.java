a78904f5f418791a765d026f919fd10f4e90f0c8
Aakanksha <aakanksha0499@gmail.com>
2022-05-17 01:26:50 +0530

    Refactored_Using_Switch-Case

--- src/employeeWage.java
+++ src/employeeWage.java
@@ -1,19 +1,32 @@
 public class employeeWage {
     // constant variables
-    static int DAILY_WORKING_HOURS = 8;
-    static int PART_TIME_HOURS = 4;
-    static int DAILY_WAGE = 20;
+    static final int DAILY_WAGE = 20;
+    static final int FULL_TIME = 1;
+    static final int PART_TIME = 2;
+
     public static void main(String[] args) {
         System.out.println("WELCOME_EMPLOYEE_TO_DAILY_WAGE_COMPUTATION");
-        int empDailyWage = DAILY_WAGE * DAILY_WORKING_HOURS;
-        int empPartTimeWage = DAILY_WAGE * PART_TIME_HOURS;
+        int workingHours = 0;
         // Computation
-        double empWage = Math.floor(Math.random() * 10) % 3;
-        if (empWage == 1)
-            System.out.println("Employee is Present Today"+ "\r\n" +"Total Wage for Today = " + empDailyWage);
-        else if (empWage == 2)
-            System.out.println("Employee is Working Part Time today = " + empPartTimeWage);
-        else
-            System.out.println("Employee is Absent Today");
+        int dayCheck = (int) (Math.round(Math.random() * 10) % 3);
+        switch (dayCheck) {
+            case FULL_TIME:
+                workingHours = 8;
+                int empWage = DAILY_WAGE * workingHours;
+                System.out.println("Employee is Working Full Time, Wage = " + empWage);
+                break;
+
+            case PART_TIME:
+                workingHours = 4;
+                int empWage1 = DAILY_WAGE * workingHours;
+                System.out.println("Employee is Working Part Time, Wage  = " + empWage1);
+                break;
+
+
+            default:
+                workingHours = 0;
+                int empWage2 = DAILY_WAGE * workingHours;
+                System.out.println("Employee is Absent, Wage = " + empWage2);
+        }
     }
 }
