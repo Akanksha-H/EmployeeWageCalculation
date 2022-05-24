5801ba8ef94996abdfb6ad2aeddfb692b5441995
Aakanksha <aakanksha0499@gmail.com>
2022-05-24 16:18:21 +0530

    Added Wages For Month

---
+++ src/EmployeeWage.java
@@ -0,0 +1,46 @@
+public class EmployeeWage {
+    // constant variables
+    static final int DAILY_WAGE = 20;
+    static final int FULL_TIME = 1;
+    static final int PART_TIME = 2;
+    public static void main(String[] args) {
+        System.out.println("WELCOME_EMPLOYEE_TO_DAILY_WAGE_COMPUTATION");
+        int workingHours = 0;
+        int totalWorkingDays = 0;
+        int totalWorkingHours = 0;
+        // Computation
+        while (totalWorkingHours < 100 && totalWorkingDays < 20) {
+            int dayCheck = (int) (Math.round(Math.random() * 10) % 3);
+
+            switch (dayCheck) {
+                case FULL_TIME:
+                    workingHours = 8;
+                    int empWage = DAILY_WAGE * workingHours;
+                    totalWorkingHours = workingHours + totalWorkingHours;
+                    totalWorkingDays++;
+                    System.out.println("Employee is Working Full Time, Wage = " + empWage );
+                    System.out.println("Employee total working hours = " + totalWorkingHours );
+
+                    if(totalWorkingHours > 100)
+                        totalWorkingHours = totalWorkingHours - workingHours;
+                    break;
+
+                case PART_TIME:
+                    workingHours = 4;
+                    int empWage1 = DAILY_WAGE * workingHours;
+                    totalWorkingHours = workingHours + totalWorkingHours;
+                    totalWorkingDays++;
+                    System.out.println("Employee is Working Part Time, Wage  = " + empWage1);
+                    System.out.println("Employee part time working hours  = " + totalWorkingHours);
+
+                    if(totalWorkingHours > 100)
+                        totalWorkingHours = totalWorkingHours - workingHours;
+                    break;
+
+
+                default:
+                    System.out.println("Employee is Absent");
+            }
+        }
+    }
+}
