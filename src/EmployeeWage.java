7b9a6a4ff3eded48f054ad6c15e7137aa9b14dbd
Aakanksha <aakanksha0499@gmail.com>
2022-05-24 16:24:28 +0530

    Added Wages For Month

--- src/EmployeeWage.java
+++ src/EmployeeWage.java
@@ -8,39 +8,41 @@ public class EmployeeWage {
         int workingHours = 0;
         int totalWorkingDays = 0;
         int totalWorkingHours = 0;
+
         // Computation
-        while (totalWorkingHours < 100 && totalWorkingDays < 20) {
+        while (totalWorkingDays < 20) {
             int dayCheck = (int) (Math.round(Math.random() * 10) % 3);
 
             switch (dayCheck) {
                 case FULL_TIME:
                     workingHours = 8;
                     int empWage = DAILY_WAGE * workingHours;
+
                     totalWorkingHours = workingHours + totalWorkingHours;
                     totalWorkingDays++;
+
                     System.out.println("Employee is Working Full Time, Wage = " + empWage );
                     System.out.println("Employee total working hours = " + totalWorkingHours );
-
-                    if(totalWorkingHours > 100)
-                        totalWorkingHours = totalWorkingHours - workingHours;
                     break;
 
                 case PART_TIME:
                     workingHours = 4;
                     int empWage1 = DAILY_WAGE * workingHours;
+
                     totalWorkingHours = workingHours + totalWorkingHours;
                     totalWorkingDays++;
+
                     System.out.println("Employee is Working Part Time, Wage  = " + empWage1);
                     System.out.println("Employee part time working hours  = " + totalWorkingHours);
-
-                    if(totalWorkingHours > 100)
-                        totalWorkingHours = totalWorkingHours - workingHours;
                     break;
 
 
                 default:
                     System.out.println("Employee is Absent");
             }
+            System.out.println(totalWorkingDays);
+            int totalWage = totalWorkingHours * DAILY_WAGE;
+            System.out.println("Wage for an month = " +totalWage);
         }
     }
 }
