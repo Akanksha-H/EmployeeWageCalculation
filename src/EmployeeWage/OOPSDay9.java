bd79f6c00cd86bd9e41a5cf22361b789640f5d64
Aakanksha <aakanksha0499@gmail.com>
2022-06-03 12:40:31 +0530

    Added: More Companies In Calculation

--- src/EmployeeWage/OOPSDay9.java
+++ src/EmployeeWage/OOPSDay9.java
@@ -4,17 +4,23 @@ public class OOPSDay9 {
     static final int FULL_TIME = 1;
     static final int PART_TIME = 2;
 
-
     public static void main(String[] args) {
         System.out.println("WELCOME EMPLOYEE TO DAILY WAGE COMPUTATION WITH OOPS");
         OOPSDay9 Maza = new OOPSDay9();
         System.out.printf(Maza.empWageCalculation(14, 50, "Maza", 40));
+
+        OOPSDay9 CocaCola = new OOPSDay9();
+        System.out.println(CocaCola.empWageCalculation(20,45,"CocaCola",45));
+
+        OOPSDay9 Sting = new OOPSDay9();
+        System.out.println(Sting.empWageCalculation(15,52,"Sting",150));
     }
 
     public String empWageCalculation (int totalWorkingDays, int totalWorkingHours, String company, int dailyWage) {
         int sumWorkingDays = 0;
         int sumWorkingHours = 0;
         int workHours = 0;
+        String company1 = company;
 
         // Computation
         while (sumWorkingHours < totalWorkingHours && sumWorkingDays < totalWorkingDays) {
@@ -25,26 +31,22 @@ public class OOPSDay9 {
                     workHours = 7;
                     int empWage = dailyWage * workHours;
                     System.out.println("Employee is Working Full Time, Wage = " + empWage);
-
                     sumWorkingHours = workHours + sumWorkingHours;
                     sumWorkingDays++;
-
                     break;
 
                 case PART_TIME:
                     workHours = 4;
                     int empWage1 = dailyWage * workHours;
                     System.out.println("Employee is Working Part Time, Wage  = " + empWage1);
-
                     sumWorkingHours = workHours + sumWorkingHours;
                     sumWorkingDays++;
-
                     break;
 
-
-                default:
+                    default:
                     System.out.println("Employee is Absent");
             }
+
             if (sumWorkingHours > totalWorkingHours)
                 sumWorkingHours = sumWorkingHours - workHours;
             System.out.println("Employee total working hours = " + sumWorkingHours);
@@ -52,10 +54,7 @@ public class OOPSDay9 {
             System.out.println("Employee working Day" + sumWorkingDays);
             int totalWage = sumWorkingHours * dailyWage;
             System.out.println("Wage for an month = " + totalWage);
-
         }
         return company;
     }
 }
-
-
